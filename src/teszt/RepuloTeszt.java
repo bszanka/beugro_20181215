9bbdedc56eae76886b8b95e096a7f4803a43a450
Balazs Szanka <szankabalazs@gmail.com>
2020-11-25 11:22:04 +0100

    Kész!

--- src/teszt/RepuloTeszt.java
+++ src/teszt/RepuloTeszt.java
@@ -7,10 +7,17 @@ import repules.Utasszallito;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
 import java.util.Scanner;
 
 public class RepuloTeszt {
+    public static void teszt(List<Repulogep> rep, String gyarto){
+        for (Repulogep r : rep) {
+            if(r.getGyarto().equalsIgnoreCase(gyarto.trim()))
+            System.out.println(r.toString());
+        }
+    }
 
     public static void main(String[] args) {
         if(args[0].length() == 0)
@@ -50,18 +57,25 @@ public class RepuloTeszt {
 //            for (Repulogep r : repgepek) {
 //                System.out.println(r);
 //            }
-
+//            7. feladat
             Legitarsasag lt = new Legitarsasag(args[1].length() > 0 ? args[1] : "Unideb Airlines",
                     repgepek.toArray(new Repulogep[repgepek.size()]));
             for (Repulogep r : repgepek) {
                 lt.felvesz(r);
             }
             Scanner stdin = new Scanner(System.in);
+            System.out.println("Adjon meg egy férőhelyet: ");
             int input = stdin.nextInt();
             for (int i = 0; i < repgepek.size(); i++) {
                 if(repgepek.get(i) instanceof Utasszallito && ((Utasszallito)repgepek.get(i)).getFerohely() >= input)
                     System.out.println(repgepek.get(i));
             }
+
+//            8. feladat
+            System.out.println("Adjon meg egy gyártót: ");
+            String input2 = stdin.next();
+            teszt(repgepek,input2);
+
         } catch (FileNotFoundException e) {
             e.printStackTrace();
             System.err.println("Nem található ilyen nevű fájl!");
